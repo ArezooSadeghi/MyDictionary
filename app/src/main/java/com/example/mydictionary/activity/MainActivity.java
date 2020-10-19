27e55907f60817b090f403a062e6d603239ee137
arezo <arezoo.sdghi@gmail.com>
2020-10-19 13:10:28 +0330

    Add menu and search item for action bar.

--- app/src/main/java/com/example/mydictionary/activity/MainActivity.java
+++ app/src/main/java/com/example/mydictionary/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mydictionary.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -27,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, WordListFragment.newInstance())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        return true;
     }
 
     public static Intent newIntent(Context context) {
